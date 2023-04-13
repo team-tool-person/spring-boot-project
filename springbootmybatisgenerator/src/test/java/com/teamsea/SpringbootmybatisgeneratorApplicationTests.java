fef744ef95f24cfbee219cc3f7a2bf27e90fba49
TeamSea <12108325+teamsea@user.noreply.gitee.com>
2023-04-14 01:55:39 +0800

    MyBatis逆向工程

---
+++ springbootmybatisgenerator/src/test/java/com/teamsea/SpringbootmybatisgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmybatisgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
