07f286fad12d6c061df04fe797a18d3780ec4927
TeamSea <lknlknlkn123@163.com>
2024-02-14 11:56:53 +0800

    未完成

--- CGB-DB-SYS-V1.01/src/test/java/com/cy/pj/dao/test/RoleDaoTest.java
+++ CGB-DB-SYS-V1.01/src/test/java/com/cy/pj/dao/test/RoleDaoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.cy.pj.common.vo.RoleMenuVo;
 import com.cy.pj.sys.dao.RoleDao;
 import com.cy.pj.sys.entity.Role;
 
@@ -38,14 +39,22 @@ public class RoleDaoTest {
         row = mapper.deleteObjects();
         log.info(row.toString());
     }
-    
+
     @Test
-    public void deleteObjectTest(){
-    	Integer row = mapper.deleteObject(0);
-    	log.info(row.toString());
-    	
-    	row = mapper.deleteObject(2);
-    	log.info(row.toString());
+    public void deleteObjectTest() {
+        Integer row = mapper.deleteObject(0);
+        log.info(row.toString());
+
+        row = mapper.deleteObject(2);
+        log.info(row.toString());
     }
 
+    @Test
+    public void findObjectByIdTest(){
+        RoleMenuVo vo = mapper.findObjectById(1);
+        
+        log.info(vo.toString());
+    }
+
+    
 }
